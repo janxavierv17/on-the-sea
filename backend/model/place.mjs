fcf5dbfa07eb5802e16afaa14a92d9110933d020
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2021-07-18 21:53:24 +1000

    Add initial frontend form for creating a place.

--- backend/model/place.mjs
+++ backend/model/place.mjs
@@ -4,11 +4,19 @@ const { Schema } = mongoose;
 // Schema
 const placeSchema = new Schema({
   name: { type: String, required: true },
-  featuredImage: { type: String, required: false },
+  typeOfPlace: {
+    type: String,
+    required: true,
+  },
+  typeOfPlaceDescription: {
+    type: String,
+    required: false,
+  },
   guests: { type: Number, required: true },
   rent: { type: Number, required: true },
   baths: { type: Number, required: true },
   beds: { type: Number, required: true },
+  featuredImage: { type: String, required: false },
   images: { type: [String], required: false },
   amenities: { type: [String], required: false },
   bookedDates: { type: [Date], required: false },
